1b73c87069befcb55e06bc2624eb310d1c8f602b
Sanchita Dhole <sanchitadhole27@gmail.com>
2024-09-02 19:26:14 +0530

    first commit

---
+++ tests/ClientApp.spec.js
@@ -0,0 +1,27 @@
+const { test, expect } = require("@playwright/test");
+
+test.only("Browser Context Playwright test", async ({ page }) => {
+  await page.goto("https://rahulshettyacademy.com/client");
+  // // await page.locator(".text-reset").click();
+  // await page.locator("#firstName").fill("Sanchita");
+  // await page.locator("#lastName").fill("Dhole");
+  // await page.locator("#userEmail").fill("abc123@gmail.com");
+  // await page.locator("#userMobile").fill("34214235");
+  // await page.locator("#userPassword").fill("Cgain@123");
+  // await page.locator("#login").click();
+
+  await page.locator("#userEmail").fill("anshika@gmail.com");
+  await page.locator("#userPassword").fill("Iamking@000");
+  await page.locator("#login").click();
+  // await page.waitForLoadState('networkidle');
+  await page.locator(".card-body b").first().waitFor();
+  const titles = await page.locator(".card-body b").allTextContents();
+  console.log(titles);
+});
+
+test("page Playwright test", async ({ page }) => {
+  await page.goto("https://google.com/");
+  //get Title - assertion
+  console.log(await page.title());
+  await expect(page).toHaveTitle("Google");
+});
---
+++ tests/UIBasicstest.spec.js
@@ -0,0 +1,56 @@
+const {test, expect} = require('@playwright/test');
+
+test.only('Browser Context Playwright test',async ({browser})=>{
+   
+
+
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    const userName=  page.locator('#username');
+    const signIn = page.locator("#signInBtn");
+    const cardTitle = page.locator(".card-body a");
+
+
+
+
+
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+    console.log(await page.title()); 
+    await userName.fill("rahulshetty");
+    await page.locator('#password').fill("learning");
+    await signIn.click();
+    // webdriver wait
+    console.log(await page.locator("[style*='block']").textContent());
+    await expect(page.locator("[style*='block']")).toContainText('Incorrect');
+    await userName.fill("");
+    await userName.fill("rahulshettyacademy")
+    await signIn.click();
+    // console.log(await cardTitle.first().textContent());
+    // console.log(await cardTitle.nth(1).textContent());
+
+    const allTitles = await cardTitle.allTextContents();
+    console.log(allTitles);
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+test('page Playwright test',async ({page})=>{
+    await page.goto('https://google.com/');
+    //get Title - assertion
+    console.log(await page.title());
+    await expect(page).toHaveTitle("Google")
+
+
+});
